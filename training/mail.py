eb3d1a8521b6949d169e560a5ee0524c433d11c3
zhoudayang <zhoudayang2@163.com>
2017-11-29 22:42:55 +0800

    training

--- training/mail.py
+++ training/mail.py
@@ -5,8 +5,8 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-sender = 'shenyu1hao@sina.com'
-receivers = ['632955628@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender = ''
+receivers = ['']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 # 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
 message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
