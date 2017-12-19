bad28558715f70c544bf4b150d3bbeca2e12c170
zhenshenyu <shenyu1hao@sina.com>
2017-12-19 21:20:56 +0800

    half

--- training/dong/dong/spiders/dongf.py
+++ training/dong/dong/spiders/dongf.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from scrapy.spiders import BaseSpider
+from scrapy.http import request
 from dong.items import DongItem
 
 
 class DongSpider(BaseSpider):
     name = 'dongf'
-    start_urls = ['http://news.people.com.cn/GB/28053/']
+    start_urls = ['http://news.people.com.cn/GB/28053']
 
     def parse(self, response):
-        for news in response.xpath('//body/center/table[@width="761"]/tbody/tr/table[@bgcolor="d2d2d2"]'):
-            try:
-
+        # news = response.xpath('//center/table/tbody/tr/td/table/tbody/tr/td/table/tbody/tr/text()').extract()
+        news = response.xpath('//center/table/tr/td/table/tr/td/table[contains(@width, "80%")]/tr').extract()
+        for new in news:
+            print new
+            print 'ok'
