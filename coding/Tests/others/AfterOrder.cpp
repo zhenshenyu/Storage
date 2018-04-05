56cc174ed86aa68a7a5ce593da7e8fd9511eb1ca
zhenshenyu <shenyu1hao@sina.com>
2018-04-05 17:01:31 +0800

    “none”

--- coding/Tests/others/AfterOrder.cpp
+++ coding/Tests/others/AfterOrder.cpp
@@ -1,42 +1,39 @@
 
 #include <iostream>
+#include <vector>
 
 
 using namespace std;
 
 
-long numPosb(const int &m,  int n){
-    if (m == 0) {
-        return n;
-    }
-    long long res=1;
-    for (int i = 1; i <= m; ++i) {
-        res*=n--;
-        res /=i;
-    }
-    return res;
-}
-
-
 int main(){
-    int k;
-    cin>>k;
-    int a,x,b,y;
-    scanf("%d",&a);
-    scanf("%d",&x);
-    scanf("%d",&b);
-    scanf("%d",&y);
-    long count=0;
-    int left = k;
-    for(int i=0;i<x;++i){
-        if (left %b==0&&left<=b*y) {
-            count+=(numPosb(i,x)*(numPosb(left/b,y)));
-        }
-        left-=a;
-        if (left <= 0) {
-            break;
+    int m,n;
+    int t1,t2;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    vector<int> x,y,z,w;
+    for (int i = 0; i < m; ++i) {
+        scanf("%d",&t1);
+        scanf("%d",&t2);
+        x.push_back(t1);
+        y.push_back(t2);
+    }
+    for (int i = 0; i < n; ++i) {
+        scanf("%d",&t1);
+        scanf("%d",&t2);
+        z.push_back(t1);
+        w.push_back(t2);
+    }
+    int vec[m][n];
+    for (int i = 0; i < m; ++i) {
+        vec[i][0] = 0;
+    }
+    for (int i = 1; i < n; ++i) {
+        vec[0][i] = 0;
+    }
+    for (int i = 1; i < m; ++i) {
+        for (int j = 1; i < n; ++j) {
+
         }
     }
-    cout<<count%1000000007;
-    return 0;
 }
