762781cadd5ddcc0034cf4e0fa60316f42423bb2
zhenshenyu <shenyu1hao@sina.com>
2018-04-15 20:55:12 +0800

    none

--- coding/Tests/others/AfterOrder.cpp
+++ coding/Tests/others/AfterOrder.cpp
@@ -1,39 +1,41 @@
-
 #include <iostream>
-#include <vector>
-
+#include <string>
 
 using namespace std;
 
-
 int main(){
-    int m,n;
-    int t1,t2;
-    scanf("%d",&m);
-    scanf("%d",&n);
-    vector<int> x,y,z,w;
-    for (int i = 0; i < m; ++i) {
-        scanf("%d",&t1);
-        scanf("%d",&t2);
-        x.push_back(t1);
-        y.push_back(t2);
-    }
-    for (int i = 0; i < n; ++i) {
-        scanf("%d",&t1);
-        scanf("%d",&t2);
-        z.push_back(t1);
-        w.push_back(t2);
-    }
-    int vec[m][n];
-    for (int i = 0; i < m; ++i) {
-        vec[i][0] = 0;
-    }
-    for (int i = 1; i < n; ++i) {
-        vec[0][i] = 0;
-    }
-    for (int i = 1; i < m; ++i) {
-        for (int j = 1; i < n; ++j) {
-
+    string steps;
+    int q;
+    getline(cin, steps);
+    scanf("%d", &q);
+    int n, m, x, y;
+    int res;
+    for (int i = 0; i < q; ++i) {
+        res=0;
+        scanf("%d", &n);
+        scanf("%d", &m);
+        scanf("%d", &x);
+        scanf("%d", &y);
+        for (char elem:steps) {
+            if (x < 1 || x > n || y < 1 || y > m) {
+                break;
+            }
+            ++res;
+            switch (elem) {
+                case 'u':
+                    y += 1;
+                    break;
+                case 'd':
+                    y -= 1;
+                    break;
+                case 'l':
+                    x -= 1;
+                    break;
+                case 'r':
+                    x += 1;
+                    break;
+            }
         }
+        cout << res << endl;
     }
 }
